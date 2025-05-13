9c5a7b25ed9a2a0033e12f838937f2644caccce5
aguschini <agusfranchini10@gmail.com>
2025-05-13 16:08:44 -0300

    Add files via upload
    Ejercicio Sanatorio

---
+++ AdministracionSanatorio-main/AdministracionSanatorio/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionSanatorio
+{
+    class Doctor
+    {
+        public string NombreCompleto { get; set; }
+        public string Matricula { get; set; }
+        public string Especialidad { get; set; }
+        public bool Disponibilidad { get; set; }
+
+        public Doctor(string nombrecompleto, string matricula, string especialidad, bool disponibilidad)
+        {
+            NombreCompleto = nombrecompleto;
+            Matricula = matricula;
+            Especialidad = especialidad;
+            Disponibilidad = disponibilidad;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Apellido} - (Matrícula: {Matricula}) - {Especialidad} - {(Disponibilidad ? "Disponible" : "No disponible")}";
+        }
+    }
+
+
+}   
---
+++ AdministracionSanatorio-main/AdministracionSanatorio/Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministracionSanatorio
+{
+    class Paciente
+    {
+        public string DocumentoIdentidad { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Telefono { get; set; }
+        public string ObraSocial { get; set; }
+        public decimal? MontoCobertura { get; set; }
+        public List<Intervencion> Intervenciones { get; private set; } = new List<Intervencion>();
+
+        public Paciente(string documentoIdentidad, string nombreCompleto, string telefono)
+        {
+            DocumentoIdentidad = documentoIdentidad;
+            NombreCompleto = nombreCompleto;
+            Telefono = telefono;
+            ObraSocial = null;
+            MontoCobertura = null;
+        }
+
+        public Paciente(string documentoIdentidad, string nombreCompleto, string telefono, string obraSocial, decimal montoCobertura) 
+            :this(documentoIdentidad, nombreCompleto, telefono)  // ← Aquí está el magic
+        {
+            ObraSocial = obraSocial;
+            MontoCobertura = montoCobertura;
+        }
+    }
+}
