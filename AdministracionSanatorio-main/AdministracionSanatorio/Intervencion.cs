e5fde6824c5339164b018f5c0740083641e206b2
aguschini <agusfranchini10@gmail.com>
2025-05-27 16:15:54 -0300

    Add files via upload
    AVANZADO

--- AdministracionSanatorio-main/AdministracionSanatorio/Intervencion.cs
+++ AdministracionSanatorio-main/AdministracionSanatorio/Intervencion.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace AdministracionSanatorio
 {
-    class Intervencion
+    class IntervencionQuirurgica
     {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Especialidad { get; set; }
+        public int Arancel { get; set; }
+
+        public Intervencion(string codigo, string desc, string especialidad, int arancel)
+        {
+            Codigo = codigo;
+            Descripcion = desc;
+            Especialidad = especialidad;
+            Arancel = aracel;
+        }
     }
+
+
+        class IntervencionPaciente
+    {
+        public string ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public IntervencionQuirurgica Intervencion { get; set; }
+        public Doctor Doc { get; set; }
+        public string DNI { get; set; }
+
+        public IntervencionPaciente(string id, Paciente paciente, Doctor doc)
+        {
+            if (Doc.Especialidad != especializacion)
+                throw new InvalidOperationException("El médico no tiene la especialidad requerida");
+
+            if(DNI )
+
+            ID = id;
+            Fecha = (DateTime.Now + 7);
+            Doctor = doc;
+            Paciente = paciente;
+            
+        }
+    }
+
+
 }
--- AdministracionSanatorio-main/AdministracionSanatorio/Program.cs
+++ AdministracionSanatorio-main/AdministracionSanatorio/Program.cs
@@ -4,6 +4,7 @@ namespace AdministracionSanatorio
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Hospital hospital = new Hospital();
